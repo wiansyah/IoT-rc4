7eb2d07351dc4c48041f6ec4f45a5b9ac10df793
Bambang Fitriadi Wiansyah <113404318+wiansyah@users.noreply.github.com>
2023-11-28 11:31:10 +0700

    Update index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -8,14 +8,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBudsWK9BzPwbKHYMHvWw-lCyOqVyRbVdQ",
-    authDomain: "enc-rc4.firebaseapp.com",
-    projectId: "enc-rc4",
-    storageBucket: "enc-rc4.appspot.com",
-    messagingSenderId: "178777703445",
-    databaseURL: "https://enc-rc4-default-rtdb.firebaseio.com/",
-    appId: "1:178777703445:web:df1d359ca5a04076993a1c",
-    measurementId: "G-0LBJMKV20T"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    databaseURL: "",
+    appId: "",
+    measurementId: ""
 };
 
 // Initialize Firebase
